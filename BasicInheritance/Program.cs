78480898259efe4d4e74e4bf175e18c7a88c8f2b
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-02-22 21:10:21 +0300

    sealed key word

--- BasicInheritance/Program.cs
+++ BasicInheritance/Program.cs
@@ -7,3 +7,6 @@ Car myCar = new Car(80) { Speed = 50 };
 Console.WriteLine($"My car is going {myCar.Speed} MPH");
 Console.ReadLine();
 
+MiniVan myVan = new MiniVan { Speed = 10 };
+Console.WriteLine($"My van is going {myVan.Speed} MPH");
+Console.ReadLine();
