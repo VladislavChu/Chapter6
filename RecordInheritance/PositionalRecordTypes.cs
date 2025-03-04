9df23f8f3cac8bda279da124a0951dfa06bee9c3
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-03-04 09:43:48 +0300

    records

---
+++ RecordInheritance/PositionalRecordTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordInheritance
+{
+    public record PositionalCar (string Make, string Model, string Color);
+    public record PositionalMiniVan (string Make, string Model, string Color, int Seating) : PositionalCar (Make, Model, Color);
+}
---
+++ RecordInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RecordInheritance;
+
+Console.WriteLine("Record type inheritance!");
+
+Car c = new Car("Honda", "Pilot", "Blue");
+MiniVan m = new MiniVan("Honda", "Pilot", "Blue", 10);
+
+Console.WriteLine($"Checking MiniVan is-a Car: {m is Car}");
+Console.ReadLine();
+
+
+PositionalCar pc = new PositionalCar("Honda", "Pilot", "Blue");
+PositionalMiniVan pm = new PositionalMiniVan("Honda", "Pilot", "Blue", 10);
+Console.WriteLine($"Checking PositionalMiniVan is-а PositionalCar: { pm is PositionalCar}");
+Console.ReadLine();
+
+
