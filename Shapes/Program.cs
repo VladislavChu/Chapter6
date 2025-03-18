3dfc2646ab492b1b79af67e1c0d9bfcaf6c5998e
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-03-18 09:00:42 +0300

    123256

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -7,3 +7,11 @@ Hexagon hex = new Hexagon("Beth");
 hex.Draw();
 Circle cir = new Circle("Cindy");
 cir.Draw();
+
+Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"), new Circle("Beth"), new Hexagon("Linda") };
+
+foreach (Shape s in myShapes)
+{
+    s.Draw();
+}
+Console.ReadLine();
