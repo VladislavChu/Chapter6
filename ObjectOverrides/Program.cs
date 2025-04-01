6a047fba2dbe2ee3e421d4fa0ef54d58b43b9025
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-04-01 09:34:15 +0300

    obj overrides

--- ObjectOverrides/Program.cs
+++ ObjectOverrides/Program.cs
@@ -3,7 +3,7 @@ using ObjectOverrides;
 
 
 Console.WriteLine("***** Fun with System.Object *****\n");
-Person p1 = new Person("Vladislav", "Chudaev", 26);
+Person p1 = new Person("Vladislav", "Chudaev", 26, "");
 
 Console.WriteLine($"ToString: {p1.ToString()}");
 Console.WriteLine($"ToString: {p1.GetHashCode()}");
@@ -18,3 +18,26 @@ if (o.Equals(p1) && p2.Equals(o))
     Console.WriteLine("Same instance!");
 }
 Console.WriteLine();
+Console.ReadLine();
+
+
+Person p3 = new Person("Homer", "Simpson", 50, "111-11-1111");
+Person p4 = new Person("Homer", "Simpson", 50, "111-11-1111");
+
+Console.WriteLine($"p3.ToString() = {p3.ToString()}");
+Console.WriteLine($"p4.ToString() = {p4.ToString()}");
+
+Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
+
+Console.WriteLine($"Same hash codes?: {p3.GetHashCode() == p4.GetHashCode()}");
+Console.WriteLine();
+
+p4.Age = 45;
+
+Console.WriteLine($"p3.ToString() = {p3.ToString()}");
+Console.WriteLine($"p4.ToString() = {p4.ToString()}");
+
+Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
+
+Console.WriteLine($"Same hash codes?: {p3.GetHashCode() == p4.GetHashCode()}");
+Console.ReadLine();
