591bb9c4f43005d4b856484f13102fa6e2e9b61b
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-04-12 11:06:36 +0300

    Simple and Custom Exceptions

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using SimpleException;
+
+Console.WriteLine("***** Fun with simple exception example *****");
+Console.WriteLine("=> Creating a car and stepping on it!");
+
+Car myCar = new Car("Zippy", 20);
+myCar.CrankTunes(true);
+
+try
+{
+    for (int i = 0; i < 10; i++)
+    {
+        myCar.Accelerate(10);
+    }
+} 
+catch (Exception e)
+{
+    Console.WriteLine("\n*** Error! ***");
+    Console.WriteLine($"Method: {e.TargetSite}");
+    Console.WriteLine($"Class defining member: {e.TargetSite.DeclaringType}");
+    Console.WriteLine($"Member type: {e.TargetSite.MemberType}");
+    Console.WriteLine($"Message: {e.Message}");
+    Console.WriteLine($"Source: {e.Source}");
+    Console.WriteLine($"Stack: {e.StackTrace}");
+    Console.WriteLine($"Help Link: {e.HelpLink}");
+}
+
+Console.WriteLine("\n***** Out of exception logic *****");
+Console.ReadLine();
